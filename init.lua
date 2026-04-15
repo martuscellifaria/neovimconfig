7dd39fb2d24dfdefa9784fc2fda7b6d13526ff83
mrtscllfr <martuscellifaria@gmx.de>
2026-04-15 20:29:20 +0200

    Added a cool theme to my neovim configuration.

--- init.lua
+++ init.lua
@@ -896,6 +896,8 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+
+      vim.cmd.colorscheme 'azuredreams'
     end,
   },
 
